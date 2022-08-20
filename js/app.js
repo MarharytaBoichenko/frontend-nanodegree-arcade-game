7f0441872300ccc2bc77c8bf2c804d7a20a4527e
MarharytaBoichenko <boichenkomv@gmail.com>
2022-08-20 13:37:08 +0300

    Add classes for player  and  enemies

--- js/app.js
+++ js/app.js
@@ -1,46 +1,110 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const FIELDWIDTH = 505;
+const FIELDHEIGHT = 606;
+const playerStartX = 203;
+const playerStartY = 300;
+const playerStepY = 83;
+const playerStepX = 101;
+const field = {
+  top: 0,
+  bottom: 606,
+  left: 0,
+  right: 505,
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+class Enemy {
+  constructor(x, y, speed, player) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.player = player;
+    this.sprite = "images/enemy-bug.png";
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  update(dt) {
+    if (this.x >= FIELDWIDTH) {
+      this.x = -100;
+    }
+    this.x += this.speed * dt;
+    this.handleCollision();
+  }
+
+  handleCollision() {
+    if (
+      Math.round(this.x) === player.x &&
+      Math.round(this.y) === player.y &&
+      Math.round(this.x) + playerStepX > player.x &&
+      Math.round(this.y) + playerStepY > player.y
+    ) {
+      player.reset();
+    }
+  }
+
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = "images/char-boy.png";
+  }
+  update() {}
 
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+  handleInput(e) {
+    switch (e) {
+      case "up":
+        this.y -= playerStepY;
+        break;
+      case "down":
+        this.y += playerStepY;
+        break;
+      case "left":
+        this.x -= playerStepX;
+        break;
+      case "right":
+        this.x += playerStepX;
+        break;
+    }
+    this.updateToStart();
+  }
 
+  reset() {
+    this.y = playerStartY;
+    this.x = playerStartX;
+  }
 
+  updateToStart() {
+    if (
+      this.y > field.bottom - playerStepY * 2 ||
+      this.y < field.top ||
+      this.x > field.right ||
+      this.x < field.left
+    ) {
+      this.reset();
+    }
+  }
+}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+const player = new Player(playerStartX, playerStartY);
+const enemyFirst = new Enemy(-100, 51, 50);
+const enemySecond = new Enemy(-100, 134, 100);
+const enemyThird = new Enemy(-100, 217, 80);
+const allEnemies = [enemyFirst, enemySecond, enemyThird];
 
-    player.handleInput(allowedKeys[e.keyCode]);
+document.addEventListener("keyup", function (e) {
+  var allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
 });
